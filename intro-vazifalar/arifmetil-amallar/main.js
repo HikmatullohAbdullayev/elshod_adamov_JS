4b46b2c1e27df73e1877ca3ddad1dd1b8692b1ce
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-08-05 00:49:40 +0500

    intial commit

---
+++ intro-vazifalar/arifmetil-amallar/main.js
@@ -0,0 +1,103 @@
+//Vazifa-6 	Do’kondan n kg nok, s kg sabzi, b kg bodring sotib olib umumiy narhni konsolga chiqaring
+
+
+        // let nok = Number(prompt("qancha nok olasiz 'kg'"))
+        // let sabzi = Number(prompt("qancha sabzi olasiz 'kg'"))
+        // let bodiring = Number(prompt("qancha bodiring olasiz 'kg'"))
+
+        // let nokNarxi = 2800
+        // let sabziNarxi = 3800
+        // let bodiringNarxi = 4800
+
+        // let umumiySumma =  nok*nokNarxi+sabzi*sabziNarxi+bodiring*bodiringNarxi 
+        // console.log(`sizdan ${umumiySumma} so'm 
+        // nok = ${nok*nokNarxi} so'm
+        // sabzi = ${sabzi*sabziNarxi} so'm
+        // bodiring = ${bodiring*bodiringNarxi} so'm`);
+
+
+//Vazifa-7  7.	A va B sonlar berilgan ularning o’rtacha qiymatini hisoblang.
+
+        // let sonA = Number(prompt("A ga son kirting"))
+        // let sonB = Number(prompt("B ga son kirting"))
+        // let sonNatija7 = (sonA+sonB)/2
+        // console.log(sonNatija7);
+
+
+//Vazifa-9 	Kvadrat tomoni berilgan. Uning perimetri hisoblansin. P = x*4
+
+        // let kvadratTomoni9 = Number(prompt("Kvadratni tomoni nechi"))
+        // let kvadratPerimetri = kvadratTomoni9 * 4
+        // console.log(`Kvadratninig perimetri ${kvadratPerimetri} sm`);
+
+
+//Vazifa-10  Kvadratning tomoni berilgan uning yuzasi topilsin.  
+
+
+        // let kvadratTomoni10 = 4
+        // let kvadratYuzasi = kvadratTomoni10 ** 2
+        // console.log(kvadratYuzasi);
+
+
+//Vazifa-11  To’g’ri to’rtburchakning tomonlari A va B berilgan. Uning yuzasi S = a*b va perimetri P = 2*(a+b) ni hisoblang
+
+
+        // let tortburchakTomoniA = Number(prompt("To'g'ri to'rtburchakni A tomonini kiriting "))
+
+        // let tortburchakTomoniB = Number(prompt("To'g'ri to'rtburchakni B tomonini kiriting "))
+
+        // let tortbutchakPerimetri = 2*(tortburchakTomoniA+tortburchakTomoniB)
+
+        // let tortburchakYuzasi = tortburchakTomoniA*tortburchakTomoniB
+
+        // alert(`To'g'ri to'rtburchakni
+        // Perimetri ${tortbutchakPerimetri} sm va Yuzasi ${tortburchakYuzasi} sm`)
+
+//Vazifa-12  Diametr D berilgan. Uning uzunligi   ni hisoblang. Pi ni 3.14 hisoblang
+
+        // const pi = 3.14
+        // let diametr = Number(prompt("Diametr kiriting"))
+        // let uzunligi = pi * diametr
+        // alert (`Aylananing uzunligi ${uzunligi}`)
+
+//Vazifa-  13.	Kub tomon a  berilgan. Kubning hajmi topilsin  va uning maydoni topilsin
+
+
+        // let kubTomoniA = Number(prompt("kubni A tomonini kiriting (sm)"))
+        // let kubHajmi = Math.pow(kubTomoniA,3)
+        // let kubMaydoni = 6 * Math.pow(kubTomoniA,2)
+        // alert(`kub hajmi ${kubHajmi} sm kub
+        // kub maydoni ${kubMaydoni} sm kvadrat`)
+
+
+//Vazifa- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ intro-vazifalar/console.log/main.js
@@ -0,0 +1,97 @@
+//  Vazifa-1  31, 18 va 79 qiymatlarni bitta probel bilan bir qatorda chiqaring. '31 18 79' matnidan foydalanmasin
+ 
+    // let a = 31
+    // let b = 18
+    // let c = 79
+    // console.log(a,b,c );
+
+// Vazifa-2.  47, 52 va 150 qiymatlarni ikkita probel bilan bir qatorda chiqaring. '47 52 150' matndan foydalanmasin
+
+    // let q = 47
+    // let w = 52
+    // let e = 150
+    // console.log(q,w,e);
+
+//Vazifa-3  50 va 10 qiymatlarini bir-birini tagidan chop eting
+
+    // let r = 50
+    // let t =10
+    // console.log(`${r}
+    // ${t}`);
+
+//Vazifa-4  5, 10 va 21 qiymatlarini bir-birini tagidan chop eting
+
+    // let y = 5
+    // let u = 10
+    // let i = 21
+    // console.log(`${y}
+    // ${u}
+    // ${i}`);
+
+
+//Vazifa-5  . Quyidagi qiymatlarni console da chiqaring:1 2
+
+    // let o = 1
+    // let p =2
+    // console.log(`${o}
+    // ${p}`);
+
+
+//Vazifa-6  Klaviaturadan raqam kiritish lozim bo'lib, kiritilgan qiymatni "Siz kiritgan son" va kiritilgan qiymat chop etilsin
+
+    // let son1 = +prompt()
+    // console.log("siz kiritgan son",son1);
+
+    
+//Vazifa-7  Foydalanuvchi ismini kiritishni va uning ismini boshidan "assalom aleykum" so'zini qo'shib chop etilsin
+
+    // let ism1 = prompt("ismingizni kiriting")
+    // console.log("assalomu alaykum " +ism1);
+
+//Vazifa-8 Foydalanuvchidan sevimli futbol komanda nomini so'rab, console da kiritilgan komanda nomi va "- chempion" javobini chiqarilsin
+
+    // let futbolKomanda = prompt("footbol jamoasi")
+    // console.log(futbolKomanda + " chempion");
+
+
+//Vazifa-9 Foydalanuvchidan butun son kiritish so'ralsin va console da undan oldingi va keyingi qiymatni console chiqarilsin. 
+// misol: 15 qiymat kiritilganda
+// 15 dan keyingi qiymat 16
+// 15 dan oldingi qiymat 14 
+
+
+    // let  son2 = Number(prompt("istalgan busun son kiritig"))
+    // let oldingiQiymat = son2-1
+    // let keyingiQiymat = son2 +1
+    // console.log(`${son2} dan oldingi qiymat ${oldingiQiymat}
+    // ${son2} dan keyingi qiymat ${keyingiQiymat}`);
+
+//Vazifa-10  console ga klaviaturadan kiritilgan ketma-ket 3 ta qiymatni ikkita probel bilan ajratib chiqarilsin
+
+    // let qiymat10_1 = prompt("1-qiymat kirting")
+    // let qiymat10_2 =  prompt("2-qiymat kirting")
+    // let qiymat10_3 =  prompt("3-qiymat kirting")
+    // console.log(`${qiymat10_1}  ${qiymat10_2}  ${qiymat10_3}`);
+
+//Vazifa-11 console da quyidagi ifodani chiqarish lozim:
+// a) 5 10      b) 100 t     v) x 25
+// 7 km         1949 v       x y
+// t,v,x va y - harflar o'rniga klaviaturadan qiymatlarni foydalanuvchi kiritishi lozim
+
+    // let qiymatT = prompt("Tga qiymat bering 'string' ")
+    // let qiymatV = prompt("V ga qiymat bering 'string' ")
+    // let qiymatX = Number(prompt("X ga son qiymat bering"))
+    // let qiymatY = Number(prompt("Y ga son qiymat bering"))
+    // console.log(`a) 5 10   b) 100 ${qiymatT}   v) ${qiymatX} 25
+    // 7 km   1949 ${qiymatV}   ${qiymatX} ${qiymatY}`);
+
+
+//Vazifa-12 console da quyidagi ifodani chiqarish lozim:
+// a) 2 kg b) a 1 v) x y
+// 13 17 19 b 5 y
+
+
+    // console.log(`a) 2 kg   b) a 1   v) x y
+    // 13  17  19  b  5  y`);
+
+    // ======TAMOM===== //
