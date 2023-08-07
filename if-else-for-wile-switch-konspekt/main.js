37f6af34ff8c56745d2c41756121f8446f257d20
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-08-07 23:23:48 +0500

    intial commit

---
+++ if-else-for-wile-switch-konspekt/main.js
@@ -0,0 +1,77 @@
+// Assignment operatorlar o'zlashtirish
+
+// "+= -= qo'shib(ayrib) tenglash a += 5 => a = a + 5
+// "*= va /=" kopaytirb tenglash va bo'lb tenglash a*=5 => a=a*5
+// va hokazo
+
+// solishtruv operatori (taqqoslash)
+
+// Taqqoslash operatorlari ikkita qiymatni solishtiradi va mantiqiy qiymatni qaytaradi, false yoki true .
+
+// bular:
+// ==  tengmi
+
+// != teng emas
+
+// === malumot turi (type) jihatdan tengmi
+
+// !==  teng lekin turi boshqa
+
+// > katta
+
+// >= katta yoki teng
+
+// < kichik
+
+// <= kichik yoki teng
+
+// mantiqiy operatorlar
+// bular:
+// && (va) har ikkita operant ham rost (true) bo'lsa rost (true) aks holda yolg'on (false) qaytaradi
+
+// || (yoki) har ikkita operantdan bittasi rost (true) bo'sa rost (true) bo'ladi
+
+// ! (inkor) rostni (true) yolgonga (false)
+//   yolgonni (false) rostga (true) aylantiradi
+
+// if else shart operatori
+// if komandasi ketidan qavs ichida kerakli shart beriladi agar shart rost (true) bo'lsa shart bajaradi yokida elseda turgan shartni bajaradi
+
+// !!!if dan keyingi qavs ichida  qavs ichida false qiymat bo'lsa ishlamaydi false qiymatlar pastda ko'rsatilgan har doim ture qiymat bo'ishi kerak!!! //
+
+// false qiymatlar:
+// NaN
+// null
+// undefined
+// 0
+// -0
+// ""
+// false
+
+        // let a =20
+        // let b =34
+
+        // if (a>b){
+        //         console.log("A B dan kartta");
+        // }
+        // else{
+        //         console.log("B A dan katta");
+        // }
+
+
+
+        // let til = prompt("til kirting");
+
+        // if (til == "uz") {
+        // console.log("assalomu alaykum");
+        // } else if (til == "ru") {
+        // console.log("nma gap");
+        // } else if (til == "en") {
+        // console.log("qalesan");
+        // } else {
+        // console.log("boshqa narsa yoz");
+        // }
+
+
+
+        
